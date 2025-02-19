e81d3141e8c00556476227c8a464cf5d2bd9f3c9
bushlalex <bushlya199920122@gmail.com>
2025-02-19 16:11:48 +0500

    Сделал всю логическую часть, игра работает на консоли

--- script.js
+++ script.js
@@ -9,23 +9,89 @@ for (i = 1; i < 10; i++) {
 
 function createPlayer(name) {
     return {
-        name: name,
+        name,
         move: []
     };
-} //создание обьекта с помощью фабричной функции
+} //создание обьекта с помощью фабричной функции 
 
 let player = player1; //присвоение первого хода первому игроку
+
 
 function makeMove(index) {   //функция делающая ход (нажатие на клетку)
     doska = doska.filter(item => item !== index);  //убирает клетку из массива doska
     if ((player1.move).includes(index)||(player2.move).includes(index)) {
         console.log("Эта клетка занята!");
+        return;
     }
     else {
         player.move.push(index);
-    }      //добавляет в массив обьекта игрока клетку, проверяет, занята ли клетка
+        (player.move).sort();
+    }      //добавляет в массив обьекта игрока клетку, проверяет, занята ли клетка + сортируем сразу, чтобы потом проверить на выигрышную комбинацию
     player = (player == player1) ? player2 : player1; //реализует передачу хода другому игроку
+
+    if (player == player1) {
+        return player2;
+    }
+    else {
+        return player1;
+    } //возвращает обьект игрока, который только что играл
 }
+
+function checkResult() {
+    let currentplayer = makeMove(2); //вместо 2 поставить documentqueryselector
+           
+    if (currentplayer == undefined) {
+        return;
+    }
+    else if  (comparison()) {
+        console.log(`Истинный гений на ${currentplayer.name} выиграл`);
+    }
+    else if (doska.length == 0) {
+        console.log('Ничья!');
+        return;
+    }
+    else {
+        let numberlol = Math.random();
+        if (numberlol < 0.33) {
+        console.log(`Какой же ход от гения на ${currentplayer.name}!`);
+        }
+        else if (numberlol > 0.66) {
+            console.log(`${currentplayer.name} гений?`);
+        }
+        else {
+            console.log(`Очень умный мув от ${currentplayer.name}!`);
+        }
+    };
+}
+
+function comparison() {
+    let winCombination = [
+        123,
+        456,
+        789,
+        147,
+        159,
+        357,
+        258,
+        369
+    ];
+    let z =  player1.move.join('');
+    let w = player2.move.join('');
+    if ((winCombination.map((item) => item == z).includes(true))||(winCombination.map((item) => item == w).includes(true))) {
+        return true;
+    }  
+    else {
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
 
 
 
